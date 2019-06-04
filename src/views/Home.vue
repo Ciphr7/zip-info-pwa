4c6a67263a672e3ccfac6773b5ad12bccd7f7b9c
kutanix <gil@cipher7designs.com>
2019-06-04 16:42:15 -0500

    updating project

---
+++ src/views/Home.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="ion-page">
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>ZipInfo</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content class="ion-padding">
+      <ZipSearch v-on:get-zip="getZipInfo" />
+      <ZipInfo v-bind:info="info" />
+    </ion-content>
+  </div>
+</template>
+
+<script>
+import ZipSearch from "../components/ZipSearch";
+import ZipInfo from "../components/ZipInfo";
+export default {
+  name: 'home',
+  components: {ZipSearch, ZipInfo},
+  data(){
+    return {
+      info: null
+    };
+  },
+  methods: {
+    async getZipInfo(zip){
+      const res = await fetch(`https://api.zippopotam.us/us/${zip}`);
+      if(res.satus == 404){
+        this.showAlert();
+      }
+      this.info = await res.json(); 
+    },
+            showAlert(){
+            return this.$ionic.alertController
+                .create({
+                    header: "Not Valid",
+                    message: "Please enter a valid US zipcode",
+                    buttons: ["OK"]
+                })
+                .then(a => a.present());
+        }
+  }
+}
+</script>
